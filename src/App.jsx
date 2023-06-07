945c4b729144166a15eaab3c864a1d12410a0b55
nanobyter <sotarfredrik@gmail.com>
2023-06-07 22:05:31 +0200

    Type-ahead search field implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,57 @@
-import { useEffect, useState } from 'react';
-import Searchbox from './Searchbox';
+import { useCombobox } from 'downshift';
+import React, { useEffect, useState } from 'react';
 
-const App = () => {
+function App() {
+  const [inputItems, setInputItems] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [singleUser, setSingleUser] = useState('');
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  const {
+    isOpen,
+    getMenuProps,
+    getInputProps,
+    getComboboxProps,
+    highlightedIndex,
+    getItemProps,
+  } = useCombobox({
+    items: inputItems,
+    onInputValueChange: ({ inputValue }) => {
+      setInputItems(
+        users.filter(item =>
+          item.name.toLowerCase().includes(inputValue.toLowerCase())
+        )
+      );
+    },
+  });
+
   return (
-    <>
-      <Searchbox />
-    </>
+    <div className='App'>
+      <h2>Current User: {singleUser}</h2>
+      <div {...getComboboxProps}>
+        <input {...getInputProps()} placeholder='Search' />
+      </div>
+      <ul {...getMenuProps()}>
+        {isOpen &&
+          inputItems.map((item, index) => (
+            <span
+              key={item.id}
+              {...getItemProps({ item, index })}
+              onClick={() => setSingleUser(item.name)}
+            >
+              <li>
+                <h4>{item.name}</h4>
+              </li>
+            </span>
+          ))}
+      </ul>
+    </div>
   );
-};
+}
 
 export default App;
--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -4,7 +4,7 @@ import { useCombobox } from 'downshift';
 const Searchbox = () => {
   const [inputItems, setInputItems] = useState([]);
   const [users, setUsers] = useState([]);
-  const [singleUser, setSingleUser] = useState([]);
+  const [singleUser, setSingleUser] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,7 +27,7 @@ const Searchbox = () => {
     isOpen,
     getMenuProps,
     getInputProps,
-    getComboProps,
+    getComboboxProps,
     highlightedIndex,
     getItemProps,
   } = useCombobox({
@@ -43,10 +43,23 @@ const Searchbox = () => {
 
   return (
     <>
-      <ul>
-        {users.map(user => {
-          return <li key={user.id}>{user.name}</li>;
-        })}
+      <h2>Current user: {singleUser}</h2>
+      <div {...getComboboxProps}>
+        <input {...getInputProps()} placeholder='Search' />
+      </div>
+      <ul {...getMenuProps()}>
+        {isOpen &&
+          inputItems.map(item => (
+            <span
+              key={item.id}
+              {...getItemProps(item)}
+              onClick={() => setSingleUser(item.name)}
+            >
+              <li>
+                <h4>{item.name}</h4>
+              </li>
+            </span>
+          ))}
       </ul>
     </>
   );
